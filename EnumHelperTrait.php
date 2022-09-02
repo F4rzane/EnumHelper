9823e292a2fd106441b9a3b37a9e5f8897cf04b7
Farzane Khazaei <47053068+F4rzane@users.noreply.github.com>
2022-09-02 15:57:26 +0430

    Create EnumHelperTrait.php

---
+++ EnumHelperTrait.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+trait EnumHelper
+{
+    /**
+     * @return array
+     */
+    public static function names(): array
+    {
+        return array_column(static::cases(), 'name');
+    }
+
+    /**
+     * @return array
+     */
+    public static function values(): array
+    {
+        return array_column(static::cases(), 'value');
+    }
+
+    /**
+     * @return array
+     */
+    public static function toArray(): array
+    {
+        return array_combine(self::names(), self::values());
+    }
+
+    /**
+     * @return array
+     */
+    public static function toArrayFlip(): array
+    {
+        return array_flip(self::toArray());
+    }
+
+    /**
+     * @return bool|string
+     */
+    public static function toJson(): bool|string
+    {
+        return json_encode(self::toArray(), JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @return bool|string
+     */
+    public static function toJsonFlip(): bool|string
+    {
+        return json_encode(self::toArrayFlip(), JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @param $value
+     * @return bool
+     */
+    public static function hasValue($value): bool
+    {
+        return !empty(static::tryFrom($value));
+    }
+
+    /**
+     * @param $key
+     * @return bool
+     */
+    public static function hasCase($key): bool
+    {
+        return array_key_exists($key, self::toArray());
+    }
+}
